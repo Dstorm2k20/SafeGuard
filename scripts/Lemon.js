e3ecf1d3b19efd4386e5035a814b5f44b95f5024
Dstorm2k20 <Dstorm2k20@gmail.com>
2025-05-21 10:30:45 -0400

    Update and rename config.js to Lemon.js

--- scripts/Lemon.js
+++ scripts/Lemon.js
@@ -2,7 +2,7 @@
 export default {
     "version": "2.0.1",
     //the owner password to edit config in game
-    "OWNER_PASSWORD": "",
+    "OWNER_PASSWORD": "1301",
     "other":{
         //dev stuff
         "consoleDebugMode": true,
@@ -15,7 +15,7 @@ export default {
     "combat": {
         "autoclicker":{
             //if player cps go over this number, player gets flagged for autoclicker
-            "maxCps": 17
+            "maxCps": 10
         },
         "killaura":{
             //if player attacks this or many more number of entities in a single tick, gets flagged for killaura
@@ -30,9 +30,9 @@ export default {
             //1 - kill player
             //2 - clear inventory
             //3 - ban player (duration specified in "punishmentTime")
-            "punishmentType": 3,
+            "punishmentType": 1,
             //enabling this will send an alert when player combat logs to everyone in game
-            "alwaysSendAlert": false,
+            "alwaysSendAlert": true,
             //valid input: <number> <day|hour|minute>
             "punishmentTime": "15 minute",
             //if admins don't get affected by anti combatlogging
@@ -82,12 +82,12 @@ export default {
             //will teleport all the players up in the air constantly
             "minBorderDistance": 500,
             //if admins can go beyond world border
-            "adminsBypassBorder": true,
+            "adminsBypassBorder": false,
         }
     },
     "chat":{
         //chat command prefix
-        "prefix": "!",
+        "prefix": "/",
         "spammer":{
             //maximum amount characters the message can have before it's considered spam
             "maxMessageCharLimit": 512,
